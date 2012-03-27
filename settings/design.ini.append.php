706d95b1229f6aefbc22626d1e02776197694fad
Frank <fneff89@gmail.com>
2012-03-27 19:53:24 +0200

    Added design for admin interface.

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -2,4 +2,12 @@
 
 [ExtensionSettings]
 DesignExtensions[]=eztotp
+
+[JavaScriptSettings]
+JavaScriptList[]=bootstrap.min.js
+BackendJavaScriptList[]=bootstrap.min.js
+
+[StylesheetSettings]
+BackendCSSFileList[]=totp.css
+BackendCSSFileList[]=system-override.css
 */ ?>
